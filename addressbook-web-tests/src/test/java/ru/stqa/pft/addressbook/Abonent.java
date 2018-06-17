33be03691f7410f8a8f73d708a9877c5de34e27b
Karl <m_loon87@mail.ru>
2018-06-17 12:39:05 +0300

    Создан тетст - создание нового контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/Abonent.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class Abonent {
+    private final String name;
+    private final String lastName;
+    private final String mobilePhone;
+    private final String email;
+    private final String address;
+
+    public Abonent(String ivan, String ivanov, String s, String s1, String moscow) {
+        name = ivan;
+        lastName = ivanov;
+        mobilePhone = s;
+        email = s1;
+        this.address = moscow;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
