0e9adb4329098b8f46090305d3fb340ed0f19279
Karl <m_loon87@mail.ru>
2018-06-17 16:25:30 +0300

    Созданы тесты для создания новой записи и удаления группы
    This reverts commit 765006d21ca0c71bca6a7e38b01d71ab10b40e3e.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AbonentTetst/Abonent.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AbonentTetst/Abonent.java
@@ -1,4 +1,4 @@
-package ru.stqa.pft.addressbook;
+package ru.stqa.pft.addressbook.AbonentTetst;
 
 public class Abonent {
     private final String name;
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AbonentTetst/AbonentCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AbonentTetst/AbonentCreationTest.java
@@ -1,4 +1,4 @@
-package ru.stqa.pft.addressbook;
+package ru.stqa.pft.addressbook.AbonentTetst;
 
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.BeforeMethod;
