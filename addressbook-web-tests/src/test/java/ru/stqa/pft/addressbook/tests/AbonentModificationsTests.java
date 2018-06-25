06c30020e00dd0db9887d02a3ea156dafd2b090b
Karl <m_loon87@mail.ru>
2018-06-26 00:04:29 +0300

    Добавлены предусловия в классы Удаления и Модификации контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AbonentModificationsTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AbonentModificationsTests.java
@@ -8,6 +8,9 @@ public class AbonentModificationsTests extends TestBase {
     @Test
     public void testAbonentModifications() {
         app.getNavigationHelper().gotoHome();
+        if (! app.getAbonentHelper().isThereAAbonent()){
+            app.getAbonentHelper().createAbonent(new AbonentData("2506_1_2","2606_1_2","+7777","@","boloto","test1"), true);
+        }
         app.getAbonentHelper().abonentSelected();
         app.getAbonentHelper().abonentModification();
         app.getAbonentHelper().fillNewAbonentForm(new AbonentData("New_2506", "new_2506", "sdds", "sdsd", "new", null), false);
