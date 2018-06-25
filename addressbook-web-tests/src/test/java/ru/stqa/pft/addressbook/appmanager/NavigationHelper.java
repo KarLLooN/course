67ed3d5c24d6be8fc98fa2006378a5a76d034d5c
Karl <m_loon87@mail.ru>
2018-06-25 23:28:57 +0300

    Оптимизация переходов между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -12,6 +12,11 @@ public class NavigationHelper extends HelperBase {
     }
 
     public void gotoGroupPage() {
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))) {
+            return;
+        }
         click(By.linkText("groups"));
     }
 
@@ -19,15 +24,19 @@ public class NavigationHelper extends HelperBase {
         click(By.linkText("add new"));
     }
 
-    public void gotoHome(){
+    public void gotoHome() {
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        }
         click(By.xpath("//div[@id='nav']//a[.='home']"));
     }
 
-    public void returnToHomePage(){
+    public void returnToHomePage() {
+
         click(By.xpath("//div[@id='content']/form[1]/input[22]"));
     }
 
-    public void closeAlert(){
+    public void closeAlert() {
         wd.switchTo().alert().accept();
     }
 }
