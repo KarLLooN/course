a7b8b7638086d2cb5b94ede3e5100efbacd3e203
Karl <m_loon87@mail.ru>
2018-07-08 15:04:41 +0300

    лямбда в creationGroup

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -22,14 +22,7 @@ public class GroupCreationTests extends TestBase {
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), befor.size() + 1);
 
-
-        int max = 0;
-        for (GroupData g : after){
-            if (g.getId() > max){
-                max = g.getId();
-            }
-        }
-        group.setId(max);
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         befor.add(group);
         Assert.assertEquals(new HashSet<Object>(befor),new HashSet<>(after));
     }
