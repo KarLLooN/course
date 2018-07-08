0c3059bb5dadee702bf86178cafd6203ce4ee1f1
Karl <m_loon87@mail.ru>
2018-07-08 13:42:57 +0300

    модиф групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -2,8 +2,10 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.AbonentData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -17,10 +19,15 @@ public class GroupModificationTests extends TestBase {
         List<GroupData> befor = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(befor.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test5", "test7"));
+        GroupData group = new GroupData(befor.get(befor.size() -1).getId(),"test1", "test5", "test7");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(befor.size(), after.size());
+
+        befor.remove(befor.size()-1);
+        befor.add(group);
+        Assert.assertEquals(new HashSet<Object>(befor),new HashSet<>(after));
     }
 }
