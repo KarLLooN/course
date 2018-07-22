c1a9d1edd83d8953f46ac1fb8dc0a79c0cce2035
Karl <m_loon87@mail.ru>
2018-07-22 14:47:20 +0300

    импорты

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AbonentCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AbonentCreationTest.java
@@ -5,13 +5,10 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.AbonentData;
 import ru.stqa.pft.addressbook.model.Abonents;
-import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
