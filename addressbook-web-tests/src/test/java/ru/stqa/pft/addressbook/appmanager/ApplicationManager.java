3b624ecfc9c6fa21633ddf36a584e796bcb17a55
Karl <m_loon87@mail.ru>
2018-07-22 16:31:29 +0300

    добавден конфиг стенда

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -8,11 +8,16 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 
 public class ApplicationManager {
 
+    private final Properties properties;
     WebDriver wd;
 
     private NavigationHelper navigationHelper;
@@ -22,19 +27,22 @@ public class ApplicationManager {
     private String browser;
 
     public ApplicationManager(String browser) {
-
         this.browser = browser;
+        properties = new Properties();
     }
 
 
-    public void init() {
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-        } else if(browser.equals(BrowserType.CHROME)) {
+        } else if (browser.equals(BrowserType.CHROME)) {
             wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.IE)){
+        } else if (browser.equals(BrowserType.IE)) {
             wd = new InternetExplorerDriver();
-        } else if (browser.equals(BrowserType.EDGE)){
+        } else if (browser.equals(BrowserType.EDGE)) {
             wd = new EdgeDriver();
         }
         wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
@@ -42,8 +50,8 @@ public class ApplicationManager {
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
         abonentHelper = new AbonentHelper(wd);
-        wd.get("http://localhost/addressbook/");
-        sessionHelper.login("admin", "secret");
+        wd.get(properties.getProperty("web.baseUrl"));
+        sessionHelper.login(properties.getProperty("web.adminLogin"),properties.getProperty("web.adminPassword"));
     }
 
 
