5c55409608cdeb622ff095a80d3eb22ac2fd5bef
Karl <m_loon87@mail.ru>
2018-07-22 15:10:43 +0300

    json

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/AbonentDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/AbonentDataGenerator.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.AbonentData;
 
@@ -42,10 +44,22 @@ public class AbonentDataGenerator {
             saveAsCsv(abonents, new File(file));
         } else if (format.equals("xml")) {
             saveAsXml(abonents, new File(file));
-        } else {
+        }
+        else if (format.equals("json")) {
+            saveAsJson(abonents, new File(file));
+        }
+        else {
             System.out.println("Unrecognized format " + format);
         }
 
+    }
+
+    private void saveAsJson(List<AbonentData> abonents, File file) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+        String json = gson.toJson(abonents);
+        Writer writer = new FileWriter(file);
+        writer.write(json);
+        writer.close();
     }
 
     private void saveAsXml(List<AbonentData> abonents, File file) throws IOException {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AbonentCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AbonentCreationTest.java
@@ -1,10 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.AbonentData;
 import ru.stqa.pft.addressbook.model.Abonents;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -19,7 +22,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class AbonentCreationTest extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validAbonents() throws IOException {
+    public Iterator<Object[]> validAbonentsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/abonents.json"));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null) {
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<AbonentData> abonents = gson.fromJson(json, new TypeToken<List<AbonentData>>(){}.getType());
+        return abonents.stream().map((a) -> new Object[]{a}).collect(Collectors.toList()).iterator();
+    }
+
+    @DataProvider
+    public Iterator<Object[]> validAbonentsFromXml() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/abonents.xml"));
         String xml = "";
         String line = reader.readLine();
@@ -33,7 +50,7 @@ public class AbonentCreationTest extends TestBase {
         return abonents.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
     }
 
-    @Test(dataProvider = "validAbonents")
+    @Test(dataProvider = "validAbonentsFromJson")
     public void testAbonentCreation(AbonentData abonent) {
 
         app.goTo().home();
