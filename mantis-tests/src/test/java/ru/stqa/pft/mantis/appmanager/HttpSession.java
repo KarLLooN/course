82aeed348794b9503a2e98bf29b3ac593dc0568c
Karl <m_loon87@mail.ru>
2018-08-02 20:29:13 +0300

    поправил баги

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -34,7 +34,7 @@ public class HttpSession {
         post.setEntity(new UrlEncodedFormEntity(params));
         CloseableHttpResponse response = httpClient.execute(post);
         String body = getTextFrom(response);
-        return body.contains(String.format("<span class=\"italic\">%s</span>", username));
+        return body.contains(String.format("<a href=\"/mantisbt-2.16.0/account_page.php\">%s</a>", username));
     }
 
     private String getTextFrom(CloseableHttpResponse response) throws IOException {
@@ -49,7 +49,7 @@ public class HttpSession {
         HttpGet get = new HttpGet(app.getProperty("web.baseUrl") + "/login.php");
         CloseableHttpResponse response = httpClient.execute(get);
         String body = getTextFrom(response);
-        return body.contains(String.format("<span class=\"italic\">%s</span>", username));
+        return body.contains(String.format("<a href=\"/mantisbt-2.16.0/account_page.php\">%s</a>", username));
     }
 
 
