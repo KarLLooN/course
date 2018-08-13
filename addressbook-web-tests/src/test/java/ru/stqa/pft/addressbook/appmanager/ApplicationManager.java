82f77259b14e0d694025d682fee30f602096ac4e
Karl <m_loon87@mail.ru>
2018-08-13 05:20:35 +0300

    поправил код - лишний пробел. все работает

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -42,7 +42,7 @@ public class ApplicationManager {
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
         dbHelper = new DbHelper();
-        if ("".equals(properties.getProperty("selenium.server "))) {
+        if ("".equals(properties.getProperty("selenium.server"))) {
             if (browser.equals(BrowserType.FIREFOX)) {
                 wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
             } else if (browser.equals(BrowserType.CHROME)) {
